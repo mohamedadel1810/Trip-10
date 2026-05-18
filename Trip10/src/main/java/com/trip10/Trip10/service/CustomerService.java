169886a4a4a56a4503c5e5d4318e67af5d59d140
mohamedadel1810 <mohamed18.adel102002@gmail.com>
2026-05-18 09:20:28 +0300

    Add files via upload

---
+++ Trip10/src/main/java/com/trip10/Trip10/service/CustomerService.java
@@ -0,0 +1,16 @@
+package com.trip10.Trip10.service;
+
+import com.trip10.Trip10.dto.*;
+
+import java.util.List;
+
+public interface CustomerService {
+
+    List<CustomerResponse> findAll();
+    ApiResponse<CustomerResponse> findById(int id);
+    ApiResponse<CustomerResponse> create(CustomerRequest request);
+    ApiResponse<CustomerResponse> update (int id, UpdateUserRequest request);
+    ApiResponse<CustomerResponse> login (CustomerLoginRequest request);
+    ApiResponse<Void> deleteById(int id);
+}
+
