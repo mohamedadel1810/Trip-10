a22a834d69ad5e697f10262fbeaaf063eaa07c00
mohamedadel1810 <mohamed18.adel102002@gmail.com>
2026-06-07 21:49:01 +0300

    Delete Trip-10-main/Trip10 directory

--- Trip-10-main/Trip10/src/main/java/com/trip10/Trip10/entity/AuthStatus.java
+++
@@ -1,8 +0,0 @@
-package com.trip10.Trip10.entity;
-
-public enum AuthStatus {
-
-    PENDING,
-    AUTHORIZED,
-    REJECTED
-}
--- Trip-10-main/Trip10/src/main/java/com/trip10/Trip10/repos/CarRepo.java
+++
@@ -1,7 +0,0 @@
-package com.trip10.Trip10.repos;
-
-import com.trip10.Trip10.entity.Cars;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarRepo extends JpaRepository<Cars,Integer> {
-}
