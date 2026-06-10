9e9a13b93efc8289407a9f309a3c25107848de97
mohamedadel1810 <mohamed18.adel102002@gmail.com>
2026-06-10 03:31:34 +0300

    Add files via upload

---
+++ Trip-10-main/src/main/java/com/trip10/Trip10/dto/CarResponse.java
@@ -0,0 +1,28 @@
+package com.trip10.Trip10.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CarResponse {
+
+
+    private int carId;
+
+    private  String color;
+
+    private String brand;
+
+    private String model;
+
+    private String plateNumber;
+
+
+
+
+}
---
+++ Trip-10-main/src/main/java/com/trip10/Trip10/entity/DocumentType.java
@@ -0,0 +1,10 @@
+package com.trip10.Trip10.entity;
+
+public enum DocumentType {
+
+    CAR_DOC,
+    PERSONAL_DOC
+
+
+
+}
