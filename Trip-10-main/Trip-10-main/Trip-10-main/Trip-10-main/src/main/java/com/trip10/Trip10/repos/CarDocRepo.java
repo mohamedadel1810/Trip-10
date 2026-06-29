cecb21a37b1c582a3be4ff53f6b57adcf9347c06
mohamedadel1810 <mohamed18.adel102002@gmail.com>
2026-06-29 12:55:41 +0300

    Delete Trip-10-main/Trip-10-main directory

--- Trip-10-main/Trip-10-main/Trip-10-main/Trip-10-main/src/main/java/com/trip10/Trip10/repos/CarDocRepo.java
+++
@@ -1,9 +0,0 @@
-package com.trip10.Trip10.repos;
-
-import com.trip10.Trip10.entity.CarDoc;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarDocRepo extends JpaRepository<CarDoc,Integer> {
-
-
-}
--- Trip-10-main/Trip-10-main/src/main/java/com/trip10/Trip10/entity/OwnerType.java
+++
@@ -1,8 +0,0 @@
-package com.trip10.Trip10.entity;
-
-public enum OwnerType {
-
-    DRIVER,
-    CAR
-
-}
