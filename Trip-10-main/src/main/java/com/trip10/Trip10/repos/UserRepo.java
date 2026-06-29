ee80a47b2ec820be7d0ee340923ee5acb03bfba8
mohamedadel1810 <mohamed18.adel102002@gmail.com>
2026-06-29 21:37:51 +0300

    Delete Trip-10-main directory

--- Trip-10-main/src/main/java/com/trip10/Trip10/repos/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package com.trip10.Trip10.repos;
-
-import com.trip10.Trip10.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User,Integer> {
-}
